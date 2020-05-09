83fa8aa16b6a53cfa98e2f1682d2ab3d540dc33e
zhanghuan <aystzh@163.com>
2020-05-09 14:18:17 +0800

    整合spring security

--- src/main/java/aystzh/com/study/config/security/SysAdminSecurityConfig.java
+++ src/main/java/aystzh/com/study/config/security/SysAdminSecurityConfig.java
@@ -1,12 +1,19 @@
 package aystzh.com.study.config.security;
 
+import aystzh.com.study.entity.security.SysResource;
 import aystzh.com.study.service.SysAdminService;
+import aystzh.com.study.service.SysResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 @EnableWebSecurity
@@ -16,9 +23,27 @@ public class SysAdminSecurityConfig extends SecurityConfig {
     @Autowired
     private SysAdminService sysAdminService;
 
+    @Autowired
+    private SysResourceService sysResourceService;
+
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> sysAdminService.loadUserByUsername(username);
     }
 
+
+    @Bean
+    public DynamicSecurityService dynamicSecurityService() {
+        return new DynamicSecurityService() {
+            @Override
+            public Map<String, ConfigAttribute> loadDataSource() {
+                Map<String, ConfigAttribute> map = new ConcurrentHashMap<>();
+                List<SysResource> resourceList = sysResourceService.findAll();
+                for (SysResource sysResource : resourceList) {
+                    map.put(sysResource.getUrl(), new org.springframework.security.access.SecurityConfig(sysResource.getId() + ":" + sysResource.getName()));
+                }
+                return map;
+            }
+        };
+    }
 }
