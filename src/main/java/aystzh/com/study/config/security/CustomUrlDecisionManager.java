19c3c9e29dd5e6854792082b9ba5ebc55ae64fdc
zhanghuan <aystzh@163.com>
2020-05-09 17:00:49 +0800

    整合spring security

--- src/main/java/aystzh/com/study/config/security/CustomUrlDecisionManager.java
+++ src/main/java/aystzh/com/study/config/security/CustomUrlDecisionManager.java
@@ -3,31 +3,28 @@ package aystzh.com.study.config.security;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Collection;
 
-
+/**
+ * 动态权限决策管理器，用于判断用户是否有访问权限
+ */
 @Component
 public class CustomUrlDecisionManager implements AccessDecisionManager {
     @Override
     public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
+        if (CollectionUtils.isEmpty(configAttributes)) {
+            return;
+        }
         for (ConfigAttribute configAttribute : configAttributes) {
             String needRole = configAttribute.getAttribute();
-            if ("ROLE_LOGIN".equals(needRole)) {
-                if (authentication instanceof AnonymousAuthenticationToken) {
-                    throw new AccessDeniedException("尚未登录，请登录!");
-                } else {
-                    return;
-                }
-            }
-            Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-            for (GrantedAuthority authority : authorities) {
-                if (authority.getAuthority().equals(needRole)) {
+            for (GrantedAuthority authority : authentication.getAuthorities()) {
+                if (authority.getAuthority().equals(needRole.trim())) {
                     return;
                 }
             }
